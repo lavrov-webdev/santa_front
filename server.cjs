f5df4a6eba8886690174a7bd9b2a4bac82eee0fb
dm.lavrov <dm.lavrov@qiwi.com>
2022-11-19 20:38:02 +0300

    fix prod server

--- server.cjs
+++ server.cjs
@@ -1,14 +1,22 @@
 const express = require('express')
 const path = require('path');
+const proxy = require('http-proxy-middleware')
 
 const app = express()
 const port = 3000
 app.use(express.static(__dirname + '/dist'));
 
+const apiProxy = proxy.createProxyMiddleware('/api', {
+  target: 'http://localhost:8080',
+  changeOrigin: true,
+  pathRewrite: { '^/api': '' }
+})
+app.use('/api', apiProxy)
+
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 })
 
 app.listen(port, () => {
-	console.log(`Server listen port ${port}`)
+  console.log(`Server listen port ${port}`)
 })
