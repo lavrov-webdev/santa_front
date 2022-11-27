f5bb831ad8d17f03000df56df299a8fbc7c68778
dm.lavrov <dm.lavrov@qiwi.com>
2022-11-27 11:06:33 +0300

    add focus on adding new user

--- src/pages/createRoom/CreateRoom.tsx
+++ src/pages/createRoom/CreateRoom.tsx
@@ -1,42 +1,43 @@
-import React, {FC} from 'react'
-import {useFieldArray, useForm} from "react-hook-form";
-import {useNavigate} from 'react-router-dom';
-import {Button, Message} from 'rsuite';
+import React, { FC } from 'react'
+import { useFieldArray, useForm } from "react-hook-form";
+import { useNavigate } from 'react-router-dom';
+import { Button, Message } from 'rsuite';
 import NameInput from './NameInput';
-import {CreateRoomFormPops} from './types';
+import { CreateRoomFormPops } from './types';
 import useAxios from "axios-hooks";
-import {useAppDispatch} from "../../store/store";
-import {CreateRoomRes, ResType} from "../../api/types";
-import {setCreatedRoom} from "../../store/roomSlice";
+import { useAppDispatch } from "../../store/store";
+import { CreateRoomRes, ResType } from "../../api/types";
+import { setCreatedRoom } from "../../store/roomSlice";
 import styles from './styles.module.scss'
 
 const CreateRoom: FC = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch()
-  const [{data: reqData, loading}, execute] = useAxios<ResType<CreateRoomRes>>({
+  const [{ data: reqData, loading }, execute] = useAxios<ResType<CreateRoomRes>>({
     method: "post",
     url: "room/create"
-  }, {manual: true})
+  }, { manual: true })
 
-  const {control, handleSubmit} = useForm<CreateRoomFormPops>({
+  const { control, handleSubmit, setFocus } = useForm<CreateRoomFormPops>({
     defaultValues: {
-      names: [{value: ""}]
+      names: [{ value: "" }]
     },
   });
-  const {fields, append, remove} = useFieldArray({
+  const { fields, append, remove } = useFieldArray({
     name: 'names',
     control,
   })
 
-  const onAppend = () => {
-    append({value: ''})
+  const onAppend = async () => {
+    await append({ value: '' })
+    setFocus(`names.${fields.length}`)
   }
 
   const onSubmit = async (formData: CreateRoomFormPops) => {
     const dataToReq = {
-      users: formData.names.map(name => ({name}))
+      users: formData.names.map(name => ({ name }))
     }
-    const {data} = await execute({
+    const { data } = await execute({
       data: dataToReq
     })
     if (!data?.error) {
@@ -54,7 +55,7 @@ const CreateRoom: FC = () => {
       <ul className={styles.formList}>
         {fields.map((item, idx) => (
           <li key={item.id} className={styles.formItem}>
-            <NameInput idx={idx} control={control}/>
+            <NameInput idx={idx} control={control} />
             <div className={styles.formItemDeleteButton}>
               <Button onClick={() => remove(idx)}>Удалить</Button>
             </div>
