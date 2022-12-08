00ebf774ccb723a98c7b14afe27dc34660994195
dm.lavrov <dm.lavrov@qiwi.com>
2022-12-08 10:55:25 +0300

    fix user to choice

--- src/pages/selectReceiver/UserToChoice.tsx
+++ src/pages/selectReceiver/UserToChoice.tsx
@@ -1,78 +1,77 @@
-import React, {FC, useState} from 'react'
-import {TUser} from '../../api/types'
-import styles from './styles.module.scss'
-import cn from 'classnames'
-import {useAppDispatch, useAppSelector} from "../../store/store";
+import { FC, useState } from "react";
+import { TUser } from "../../api/types";
+import styles from "./styles.module.scss";
+import cn from "classnames";
+import { useAppDispatch, useAppSelector } from "../../store/store";
 import Confetti from "react-confetti";
 import useWindowSize from "react-use/lib/useWindowSize";
-import {selectUser} from "../../store/usersSlice";
-import {useSelectUser} from "../../api";
+import { selectUser } from "../../store/usersSlice";
+import { useSelectUser } from "../../api";
 
 type UserToChoiceProps = {
-  id: number,
-  hasOpenedCard: boolean,
-  onSelect: (v: boolean) => void
-} & Omit<TUser, "id">
+  id: number;
+  hasOpenedCard: boolean;
+  onSelect: (v: boolean) => void;
+} & Omit<TUser, "id">;
 
-const UserToChoice: FC<UserToChoiceProps> = ({id, name, hasOpenedCard, onSelect}) => {
-  const [isOpen, setIsOpen] = useState(false)
+const UserToChoice: FC<UserToChoiceProps> = ({
+  id,
+  name,
+  hasOpenedCard,
+  onSelect,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
   const [isConfettiRunning, setIsConfettiRunning] = useState(false);
   const dispatch = useAppDispatch();
-  const loggedUser = useAppSelector(store => store.users.loggedUser)
-  const selectedUser = useAppSelector(store => store.users.selectedUser)
-  const roomId = useAppSelector(state => state.room.roomId)
-  const {width, height} = useWindowSize()
-  const [_, sendSelectUser] = useSelectUser()
+  const loggedUser = useAppSelector((store) => store.users.loggedUser);
+  const selectedUser = useAppSelector((store) => store.users.selectedUser);
+  const roomId = useAppSelector((state) => state.room.roomId);
+  const { width, height } = useWindowSize();
+  const [_, sendSelectUser] = useSelectUser();
 
   const selectHandler = async () => {
-    if (!selectedUser || !loggedUser || !roomId) return
-    onSelect(true)
-    setIsOpen(true)
-    const {data} = await sendSelectUser({
+    if (!loggedUser || !roomId) return;
+    onSelect(true);
+    setIsOpen(true);
+    const { data } = await sendSelectUser({
       data: {
-        selected_user_id: selectedUser.id,
+        selected_user_id: id,
         choosing_user_id: loggedUser,
-        room_id: roomId
-      }
-    })
+        room_id: roomId,
+      },
+    });
     if (data.error) {
-      onSelect(false)
-      setIsOpen(false)
+      onSelect(false);
+      setIsOpen(false);
     } else {
-      setIsConfettiRunning(true)
+      setIsConfettiRunning(true);
       setTimeout(() => {
-        setIsConfettiRunning(false)
-      }, 3000)
-      dispatch(selectUser({id, name}))
+        setIsConfettiRunning(false);
+      }, 3000);
+      dispatch(selectUser({ id, name }));
     }
-  }
+  };
   return (
     <>
       <div
-        className={cn(
-          styles.userWrapper,
-          {[styles.Unactive]: hasOpenedCard}
-        )}
+        className={cn(styles.userWrapper, { [styles.Unactive]: hasOpenedCard })}
         onClick={hasOpenedCard ? undefined : selectHandler}
       >
-        <div className={cn(
-          styles.userInner,
-          {[styles.Active]: isOpen}
-        )}>
+        <div className={cn(styles.userInner, { [styles.Active]: isOpen })}>
           <div className={styles.userFront}>Выбери меня!</div>
           <div className={styles.userBack}>{name}</div>
         </div>
       </div>
-      {
-        !!selectedUser && <Confetti
-					width={width}
-					height={height}
-					numberOfPieces={500}
-					recycle={isConfettiRunning}
-				/>
-      }
+      {!!selectedUser && (
+        <Confetti
+          width={width}
+          height={height}
+          numberOfPieces={500}
+          recycle={isConfettiRunning}
+        />
+      )}
     </>
-  )
-}
+  );
+};
 
 export default UserToChoice;
