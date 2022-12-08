439886086f55b08b09fe25a9ab69bf0124d1c02c
dm.lavrov <dm.lavrov@qiwi.com>
2022-12-08 20:34:06 +0300

    commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
-import "rsuite/dist/rsuite.min.css";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
+import 'rsuite/dist/rsuite.min.css'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import { useState } from 'react'
+import { Input } from 'rsuite'
 
 function App() {
+  const [value, setValue] = useState('')
   return (
     <Provider store={store}>
       <RouterProvider router={router} />
+      <Input value={value} onChange={setValue} />
     </Provider>
-  );
+  )
 }
 
-export default App;
+export default App
