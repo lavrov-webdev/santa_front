7eddb6535156e2cb6b51b79209f845c38443f24b
dm.lavrov <dm.lavrov@qiwi.com>
2022-12-23 21:15:07 +0300

    remove old code

--- src/pages/createRoom/utils.ts
+++ src/pages/createRoom/utils.ts
@@ -1,17 +1,3 @@
 // TO-DO поменять хардкод ip на переменную
-import { CreateRoomFormFields } from './types'
-import { isNumber, uniqueArray } from '../../utils/validation'
-
 export const createRoomLink = (roomId: string) =>
   `http://82.146.57.74/room/${roomId}`
-
-export const validateCreateRoomFrom = (formValues: CreateRoomFormFields) => {
-  const errors: { [T in keyof CreateRoomFormFields]?: string } = {}
-
-  if (uniqueArray(formValues.names))
-    errors.names = 'Имена должны быть уникальными'
-  if (formValues.cost && isNumber(formValues.cost))
-    errors.cost = 'Введите корректную сумму'
-
-  return errors
-}
