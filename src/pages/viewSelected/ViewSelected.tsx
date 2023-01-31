4899fa1d178201863066d34863f816e2c828f720
dm.lavrov <dm.lavrov@qiwi.com>
2023-01-31 20:39:06 +0300

    fix view-selected redirect

--- src/pages/viewSelected/ViewSelected.tsx
+++ src/pages/viewSelected/ViewSelected.tsx
@@ -1,20 +1,16 @@
-import { FC, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { FC } from 'react'
 import { useAppSelector } from '../../store/store'
 import styles from './stlyles.module.scss'
 
 const ViewSelected: FC = () => {
   const recipient = useAppSelector((state) => state.account.recipient)
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    navigate('/')
-  }, [recipient])
 
   return (
     <div className={styles.index}>
       <h4 className={styles.indexHeader}>
-        Вы уже выбрали пользователя {recipient?.name}
+        {!recipient
+          ? 'Вы пока никого не выбрали'
+          : `Вы уже выбрали пользователя ${recipient.name}`}
       </h4>
     </div>
   )
