dc32560dc4400376d54fc0a452bc69eeff1451dc
root <root@lavrov.webdev.fvds.ru>
2023-10-29 09:27:41 +0300

    change port

--- server.cjs
+++ server.cjs
@@ -1,20 +1,20 @@
 const express = require('express')
-const path = require('path');
+const path = require('path')
 const proxy = require('http-proxy-middleware')
 
 const app = express()
-const port = 3000
-app.use(express.static(__dirname + '/dist'));
+const port = 3100
+app.use(express.static(__dirname + '/dist'))
 
 const apiProxy = proxy.createProxyMiddleware('/api', {
   target: 'http://localhost:8080',
   changeOrigin: true,
-  pathRewrite: { '^/api': '' }
+  pathRewrite: { '^/api': '' },
 })
 app.use('/api', apiProxy)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
 app.listen(port, () => {
